d5d8b7f59e6005c4163ad7f487bbece0230d187f
sghctoma <sghctoma@gmail.com>
2024-11-05 21:47:23 +0100

    Add script to test functionality without a Pi

---
+++ mock.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env -S GPIOZERO_PIN_FACTORY=mock GPIOZERO_MOCK_PIN_CLASS=mockpwmpin python3
+
+import logging
+import time
+import curses
+import clevertoad
+
+
+class CursesHandler(logging.Handler):
+    def __init__(self, screen):
+        logging.Handler.__init__(self)
+        self.screen = screen
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            screen = self.screen
+            fs = "\n%s"
+            screen.addstr(fs % msg)
+            screen.refresh()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except (Exception):
+            self.handleError(record)
+
+
+def press(pin):
+    pin.drive_low()
+    time.sleep(0.05)
+    pin.drive_high()
+
+
+def longpress(pin):
+    pin.drive_low()
+    time.sleep(1)
+    pin.drive_high()
+
+
+def main(stdscr):
+    stdscr.clear()
+    stdscr.scrollok(True)
+    stdscr.addstr("insert coin: press c\n")
+    stdscr.addstr("pull lever:  press l\n")
+    stdscr.addstr("dice mode:   press d\n")
+    stdscr.addstr("quit:        press q\n")
+    stdscr.addstr("====================\n")
+
+    handler = CursesHandler(stdscr)
+    clevertoad.logger.propagate = False
+    clevertoad.logger.addHandler(handler)
+
+    toad = clevertoad.CleverToad()
+    while True:
+        key = stdscr.getch()
+        if key == ord('q'):
+            break
+        elif key == ord('c'):
+            press(toad.coin_button.pin)
+        elif key == ord('l'):
+            press(toad.lever_button.pin)
+        elif key == ord('d'):
+            longpress(toad.lever_button.pin)
+
+
+curses.wrapper(main)
